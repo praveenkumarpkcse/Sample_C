39a42dec57c73167fe4d8403b9eb16a349e50580
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-20 15:19:31 +0530

    Create amst_interval.c

---
+++ amst_interval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+ int no1,no2,i,sum,r;
+ int n;
+ scanf("%d%d",&no1,&no2);
+ 
+ for(i=1;i<=500;i++)
+ { 
+    sum=0;
+    n=i;
+   while(n!=0)
+   {
+       r=n%10;
+       sum=sum+(r*r*r);
+       n=n/10;
+   }
+ if(i==sum)
+printf("%d ",i);
+ }
+
+}
