6e5391b6fc13eb4a4c77b8b4e6031fe2fca8897e
praveenkumarpkcse <50762992+praveenkumarpkcse@users.noreply.github.com>
2019-05-20 12:36:54 +0530

    Create muplti.c

---
+++ muplti.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    int no;
+    scanf("%d",&number);
+    for(int i=1;i<=5;i++)
+    {
+        no=number*i;
+        printf("%d ",no);
+    }
+ 
+}
